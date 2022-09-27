f215ce2180913a0c71194a882a813cb4d44f1fda
BurhanUlHaqq <bhzahir@gmail.com>
2022-09-27 12:46:58 +0500

    Initial commit

---
+++ src/main/java/com/firstsprintapp/firstsprintapp/FirstSprintAppApplication.java
@@ -0,0 +1,13 @@
+package com.firstsprintapp.firstsprintapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSprintAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSprintAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstsprintapp/firstsprintapp/FirstSprintAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstsprintapp.firstsprintapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSprintAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
